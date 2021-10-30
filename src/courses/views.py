657dfe7848f843f031bc2501af15d23d76aef299
Esat Koç <esat.koc@metu.edu.tr>
2021-10-30 10:31:32 +0300

    Class based view test

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Course
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course/list.html'
+    context_object_name = 'courses'
